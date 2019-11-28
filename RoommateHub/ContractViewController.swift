aa286acf091a40fbbbdc19f2dd361d6649d2c04e
Jeremy Hsu <jeremyhsu1@gmail.com>
2019-11-28 14:47:05 -0800

    image upload functionality

--- RoommateHub/ContractViewController.swift
+++ RoommateHub/ContractViewController.swift
@@ -10,12 +10,32 @@ import Foundation
 
 import UIKit
 
-class ContractViewController: UIViewController {
-    @IBOutlet var contractTitle: UILabel!
+class ContractViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var contractTitle: UILabel!
+    
+    @IBOutlet weak var contractImage: UIImageView!
+    
+    
+    @IBAction func selectPhoto(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let picker = UIImagePickerController()
+            
+            picker.delegate = self
+            picker.sourceType = .photoLibrary
+            picker.allowsEditing = false
+            
+            present(picker, animated: true, completion: nil)
+        }
+    }
     
     @IBAction func about(_ sender: Any) {
         performSegue(withIdentifier: "about", sender: sender)
     }
-
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        dismiss(animated: true, completion: nil)
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            contractImage.image = image 
+        }
+    }
 }
