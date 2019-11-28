785e1e779cdf29e9d36d5f7c0c8a32f4d864e063
Geena Kim <geena@Geenas-Air.fios-router.home>
2019-11-28 11:53:30 -0500

    framework for homepage

--- RoommateHub/ViewController.swift
+++ RoommateHub/ViewController.swift
@@ -6,14 +6,34 @@
 //  Copyright © 2019 Jeremy Hsu. All rights reserved.
 //
 
+//this view controller is for the HOMEPAGE and just directs you to all the other storyboards
+//to do: segues lol
+
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var homePageList: [String] = []
+    
+    
+    @IBOutlet var hpTitle: UILabel! //hp stands for homepage
+    @IBOutlet var hpRoommate: UIButton!
+    @IBOutlet var hpHouseKeeping: UIButton!
+    @IBOutlet var hpMessageBoard: UIButton!
+    @IBOutlet var hpContract: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        hpTitle.text = "Roommate Hub"
+    
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
 
 
 }
