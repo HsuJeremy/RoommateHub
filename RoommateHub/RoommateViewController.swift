be69bd523f09a0fa8210dc035c0d728babcf5a0f
Jeremy Hsu <jeremyhsu1@gmail.com>
2019-12-03 19:44:46 -0500

    RoommateViewController now displays profile information

--- RoommateHub/RoommateViewController.swift
+++ RoommateHub/RoommateViewController.swift
@@ -12,8 +12,22 @@ import UIKit
 class RoommateViewController: UIViewController {
     var roommate: Roommate? = nil
     
+    @IBOutlet weak var fullNameLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var hometownLabel: UILabel!
+    @IBOutlet weak var concentrationLabel: UILabel!
+    @IBOutlet weak var gradYearLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        fullNameLabel.text = roommate!.firstName + " " + roommate!.lastName
+        usernameLabel.text = roommate!.username
+        hometownLabel.text = roommate!.hometown
+        concentrationLabel.text = roommate!.concentration
+        gradYearLabel.text = String(roommate!.year)
+        ageLabel.text = String(roommate!.age)
+        
     }
     
     override func viewWillDisappear(_ animated: Bool) {
