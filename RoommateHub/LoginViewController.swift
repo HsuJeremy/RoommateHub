c92043eedc020920860b041615e7a7ac321eed62
Jeremy Hsu <jeremyhsu1@gmail.com>
2019-12-03 22:00:54 -0500

    Random tests

---
+++ RoommateHub/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  RoommateHub
+//
+//  Created by Jeremy Hsu on 12/3/19.
+//  Copyright © 2019 Jeremy Hsu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var userTextField: UITextField!
+    
+    
+}
