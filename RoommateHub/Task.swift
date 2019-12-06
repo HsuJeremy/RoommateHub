ff1fd51c1287edf554c8e3f2914aa286a661e45e
Geena Kim <geena@dhcp-10-250-136-253.harvard.edu>
2019-12-06 14:47:08 -0500

    table set up

---
+++ RoommateHub/Task.swift
@@ -0,0 +1,16 @@
+//
+//  Task.swift
+//  RoommateHub
+//
+//  Created by Geena Kim on 12/5/19.
+//  Copyright © 2019 Jeremy Hsu. All rights reserved.
+//
+
+import Foundation
+import SQLite3
+import FirebaseDatabase
+
+struct Task {
+    var name = ""
+    var important = false
+}
---
+++ RoommateHub/TaskViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TaskViewController.swift
+//  RoommateHub
+//
+//  Created by Geena Kim on 12/6/19.
+//  Copyright © 2019 Jeremy Hsu. All rights reserved.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
